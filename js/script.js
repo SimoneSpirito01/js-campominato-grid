332279eb0798f4a9af25ec0f7bd11c3249c0bf5b
SimoneSpirito <simospiri01@gmail.com>
2021-11-18 12:12:43 +0100

    ottimizzazione codice con aggiunta funzioni

--- js/script.js
+++ js/script.js
@@ -5,50 +5,14 @@ submit.addEventListener('click', function(){
     const select = document.querySelector('select');
     const container = document.querySelector('.container');
     console.log(select.value);
-
+    // pulisco il container e aggiungio i quadrati
     container.innerHTML = '';
     addSquare(select.value,container);
-
+    // aggiungo le bombe
     const squares = document.getElementsByClassName('square');
-
-    for (let i = 0; i < 16; i++){
-        let y = Math.floor(Math.random() * squares.length) + 1;
-        console.log(i , y);
-        while (squares[y - 1].classList.contains('bomb')){
-            y += 1;
-            console.log('già bomba');
-        }
-        squares[y - 1].classList.add('bomb');
-    }
-
-    let tentativi = 0;
-    let sentinella = false;
-    
-    for (let i = 0; i < squares.length; i++){
-
-        squares[i].addEventListener('click', function(){
-            if (sentinella == false){
-                if (this.classList.contains('bomb')){
-                    alert('Hai perso');
-                    const punteggio = document.querySelector('.punteggio');
-                    punteggio.innerHTML = 'Complimenti! il tuo punteggio è: ' + tentativi;
-                    const bombs = document.getElementsByClassName('bomb');
-                    for (let j = 0; j < bombs.length; j++){
-                        bombs[j].classList.add('red');
-                    }
-                    submit.innerHTML = 'Riprova';
-                    sentinella = true;
-                    console.log(sentinella);
-                } else if (this.classList.contains('blue')){
-                    alert('ATTENZIONE! seleziona una casella diversa')
-                } else {
-                    this.classList.add('blue');
-                    tentativi++;
-                }
-            }
-        });
-    }
-
+    addBomb(squares);
+    // aggiungo la funzione che mi permette di giocare
+    game(squares,submit);
 });
 
 function addSquare(type,where){
@@ -68,5 +32,47 @@ function addSquare(type,where){
         square.innerText = i + 1;
         square.className = `square ${type}`;
         where.append(square);
+    }
+}
+
+function addBomb(where){
+    for (let i = 0; i < 16; i++){
+        let y = Math.floor(Math.random() * where.length) + 1;
+        console.log(i , y);
+        while (where[y - 1].classList.contains('bomb')){
+            y += 1;
+            console.log('già bomba');
+        }
+        where[y - 1].classList.add('bomb');
+    }
+}
+
+function game(where,button){
+    let tentativi = 0;
+    let sentinella = false;
+    
+    for (let i = 0; i < where.length; i++){
+
+        where[i].addEventListener('click', function(){
+            if (sentinella == false){
+                if (this.classList.contains('bomb')){
+                    alert('Hai perso!');
+                    const punteggio = document.querySelector('.punteggio');
+                    punteggio.innerHTML = 'Complimenti! il tuo punteggio è: ' + tentativi;
+                    const bombs = document.getElementsByClassName('bomb');
+                    for (let j = 0; j < bombs.length; j++){
+                        bombs[j].classList.add('red');
+                    }
+                    button.innerHTML = 'Riprova';
+                    sentinella = true;
+                    console.log(sentinella);
+                } else if (this.classList.contains('blue')){
+                    alert('ATTENZIONE! seleziona una casella diversa')
+                } else {
+                    this.classList.add('blue');
+                    tentativi++;
+                }
+            }
+        });
     }
 }
